fix(phys): ReciprocalNavigation: fix hkl calculation array for allowedReflections_G
Addresses #29: Bragg reflections at high Q sometimes not found

orgui/datautils/xrayutils/ReciprocalNavigation.py
@@ -105,13 +105,13 @@ def allowedReflections_G(xtal: Union[CTRcalc.SXRDCrystal,CTRcalc.UnitCell],maxQ:
         b = xtal.b
     hklmax = np.floor(maxQ/b).astype(np.int64)
     if 'negative' in keyargs and keyargs['negative']:
-        h = np.arange(-hklmax[0],hklmax[0])
-        k = np.arange(-hklmax[1],hklmax[1])
-        l = np.arange(-hklmax[2],hklmax[2])
+        h = np.arange(-hklmax[0],hklmax[0]+1)
+        k = np.arange(-hklmax[1],hklmax[1]+1)
+        l = np.arange(-hklmax[2],hklmax[2]+1)
     else:
-        h = np.arange(hklmax[0])
-        k = np.arange(hklmax[1])
-        l = np.arange(hklmax[2])
+        h = np.arange(hklmax[0]+1)
+        k = np.arange(hklmax[1]+1)
+        l = np.arange(hklmax[2]+1)
         
     keyargs['hklrange'] = (h,k,l)
     